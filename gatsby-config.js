ba0f8d4a5464ea3ae0554cac4decf6295effd02e
Justin <Justin>
2020-10-23 23:38:36 +0800

    changes-10-23-2020

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `All That Is`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images`, 
       },
     },
     `gatsby-transformer-sharp`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -24,24 +24,26 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+    <>  
+      <Header siteTitle={data.site.siteMetadata?.title || `All That Is`} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 1250,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+
+        <footer style={{marginTop: `2rem`}}>
+          <p>
+            * Hint: all the really B-I-G expriences happen 
+            in either the Wilderness, the Forest, or on the Mountain
+          </p>
         </footer>
       </div>
+
+
     </>
   )
 }
